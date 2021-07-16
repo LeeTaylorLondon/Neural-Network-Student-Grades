8393f000c37a84a2dfa838db8ee338185626fb03
Lee <lees_taylor_18@yahoo.com>
2021-07-16 14:48:12 +0100

    Added model to predict overall grade Updated function preprocess_report Added function preprocess_all to PP all grades Added comments to function build_model_r3 Added function model_predict to reduce code duplication Added test_report function Added test_all function Updated main section File deprecated.py contains old model structures and beginning code for record

--- testing.py
+++ testing.py
@@ -1,9 +1,12 @@
+# Author: Lee Taylor
+# Personal project to check what I've learned from my previous
+# ML & NN projects https://github.com/LeeTaylorNewcastle/
 import numpy as np
 from tensorflow.keras.layers import Dense, LeakyReLU
 from tensorflow.keras.models import Sequential
 
 
-# --[1st Year Grades (Shit)]--
+# --[1st Year Grades (bad)]--
 report = [65, 84, 74]
 coding = [87.5, 94, 65, 55, 57, 55, 60, 100, 84, 80]
 exam = [90, 90, 90]
@@ -17,46 +20,66 @@ output2 = [87.3]
 
 
 def preprocess_report(rep, rep2):
-    report.append(sum(rep) / len(rep))
-    rv = np.asarray([rep], dtype="int16")
-    rv2 = np.asarray([rep2], dtype="int16")
+    """ Processes lists containing report grades """
+    rv = np.asarray([rep], dtype="float32")
+    rv2 = np.asarray([rep2], dtype="float32")
     return rv, rv2
 
-# def build_report_model_1():
-#     model = Sequential()
-#     model.add(Dense(units=32, activation='relu', input_dim=4))
-#     model.add(Dense(units=16, activation='relu'))
-#     model.add(Dense(units=8, activation='relu'))
-#     model.add(Dense(units=4, activation='relu'))
-#     model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
-#     return model
+def preprocess_all():
+    """ Processes all lists of grades by concatenating them """
+    return np.asarray([np.concatenate((report, coding, exam, output))], dtype="float32")
 
-def build_report_model_2():
+def build_model_r3(input_shape, output_shape):
+    """ Model for predicting report grades. Note the activation
+    is LeakyReLU because ReLU would pre-emptively drop units
+    leading to incorrect predictions of [0. 0. ...].
+     The 'r' in 'r3' denotes 'report', '3' denotes version. """
     model = Sequential()
-    model.add(Dense(units=32, input_dim=4))
+    model.add(Dense(units=32, input_dim=input_shape))
     model.add(LeakyReLU())
     model.add(Dense(units=16))
     model.add(LeakyReLU())
     model.add(Dense(units=8))
     model.add(LeakyReLU())
-    model.add(Dense(units=4))
+    model.add(Dense(units=output_shape))
     model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
     return model
 
+def model_predict(model, inp, ans):
+    """ Passed model outputs it's prediction and extra data
+     such as the input passed, and the output rounded """
+    print(f"\nModel.predict({inp})")
+    predict = model.predict(inp)
+    # Out data
+    print(f"{predict} Model prediction\n{ans} Answer")
+    for i, g in enumerate(predict[0]): predict[0][i] = round(g, 1)
+    print(f"{predict} Model prediction rounded")
 
-if __name__ == '__main__':
+def test_report(verbose=True):
+    """ Preprocess report grades, build model for reports, fit model,
+    query trained/fitted model on input and output it's guess """
     debug = True
     r, r2, = preprocess_report(report, report2)
-    report_model = build_report_model_2()
-    # Debug
+    report_model = build_model_r3(3, 4)
+    if debug: print(f"report shapes: {r.shape}, {r2.shape}")
+    # Fit model and predict
+    report_model.fit(r, r2, epochs=200,verbose=verbose)
+    if verbose: model_predict(report_model, r, r2)
+
+def test_all(debug=False):
+    """ Preprocess all grades, build model suited for all grades, fit model,
+    query trained/fitted model on input and output it's guess """
+    inp = preprocess_all()
+    out = np.asarray([output2[0]], dtype="float32")
     if debug:
-        print(f"report shapes: {r.shape}, {r2.shape}")
-    # Debug
-    report_model.fit(r, r2, epochs=200)
-    predict = report_model.predict(r)
-    # Out data
-    print()
-    print(predict)
-    print(r2)
-    for i,g in enumerate(predict[0]): predict[0][i] = round(g, 1)
-    print(predict)
+        print(inp, out)
+        print(inp.shape, out.shape)
+        print(inp.shape[0])
+    model = build_model_r3(inp.shape[1], 1)
+    model.fit(inp, out, epochs=200)
+    model_predict(model, inp, out)
+
+
+if __name__ == '__main__':
+    test_report(verbose=True)
+    test_all(debug=True)
