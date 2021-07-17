a7c56b96a73d81f55bef162f7ac4b5fa625a9bda
Lee <lees_taylor_18@yahoo.com>
2021-07-17 23:08:30 +0100

    Trained model to predict output within 2.04 points Todo: adjusting model layers to increase model testing accuracy

--- testing_student_grade.py
+++ testing_student_grade.py
@@ -4,7 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import scale
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
-from testing_my_grades import build_model_a1
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LeakyReLU
 
@@ -38,8 +37,24 @@ def data_preprocessing(df):
 def split_data(df):
     x = df.loc[:, df.columns != 'G3']
     y = df.loc[:, 'G3']
-    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.2)
+    x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=0.1)
     return x_train, x_test, y_train, y_test
+
+def build_model(input_shape, output_shape=1):
+    model = Sequential()
+    model.add(Dense(units=256, input_dim=input_shape))
+    model.add(LeakyReLU())
+    model.add(Dense(units=256))
+    model.add(LeakyReLU())
+    model.add(Dense(units=192))
+    model.add(LeakyReLU())
+    model.add(Dense(units=128))
+    model.add(LeakyReLU())
+    model.add(Dense(units=1))
+    model.add(LeakyReLU())
+    model.add(Dense(units=output_shape))
+    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+    return model
 
 def test_model(model, *data):
     if len(data) != 4: raise TypeError('Incorrect amount of data passed to test_model(...).')
@@ -56,8 +71,14 @@ if __name__ == '__main__':
     gdf = load_data()
     # clean_data(df)
     gdf, _ = data_preprocessing(gdf)
-    print(gdf)
-    # x_train, x_test, y_train, y_test = split_data(gdf)
-    # model = build_model_a1(33)
-    # model.fit(x_train, y_train, epochs=20)
-    # test_model(model, x_train, x_test, y_train, y_test)
+    # print(gdf)
+    x_train, x_test, y_train, y_test = split_data(gdf)
+    model = build_model(32)
+    model.fit(x_train, y_train, epochs=20, batch_size=1)
+
+    # try:
+    #     model.evaluate(y_train, y_test)
+    # except ValueError as e:
+    #     print(f"\nERROR: {e}\n")
+
+    test_model(model, x_train, x_test, y_train, y_test)
