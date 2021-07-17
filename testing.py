eaca3f50462ba1da8be742af56ab69f6488a49fb
Lee <lees_taylor_18@yahoo.com>
2021-07-17 13:58:42 +0100

    Added model for all 17 grades Imported pandas and matplotlib to plot grades

--- testing.py
+++ testing.py
@@ -1,7 +1,10 @@
 # Author: Lee Taylor
 # Personal project to check what I've learned from my previous
-# ML & NN projects https://github.com/LeeTaylorNewcastle/
+# ML & NN projects on https://github.com/LeeTaylorNewcastle/
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 from tensorflow.keras.layers import Dense, LeakyReLU
 from tensorflow.keras.models import Sequential
 
@@ -45,6 +48,22 @@ def build_model_r3(input_shape, output_shape):
     model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
     return model
 
+def build_model_a1(input_shape, output_shape=1):
+    model = Sequential()
+    model.add(Dense(units=128, input_dim=input_shape))
+    model.add(LeakyReLU())
+    model.add(Dense(units=64))
+    model.add(LeakyReLU())
+    model.add(Dense(units=64))
+    model.add(LeakyReLU())
+    model.add(Dense(units=32))
+    model.add(LeakyReLU())
+    model.add(Dense(units=1))
+    model.add(LeakyReLU())
+    model.add(Dense(units=output_shape))
+    model.compile(optimizer="adam", loss="mse", metrics=['accuracy'])
+    return model
+
 def model_predict(model, inp, ans):
     """ Passed model outputs it's prediction and extra data
      such as the input passed, and the output rounded """
@@ -54,6 +73,7 @@ def model_predict(model, inp, ans):
     print(f"{predict} Model prediction\n{ans} Answer")
     for i, g in enumerate(predict[0]): predict[0][i] = round(g, 1)
     print(f"{predict} Model prediction rounded")
+    print(model.evaluate(inp, ans))
 
 def test_report(verbose=True):
     """ Preprocess report grades, build model for reports, fit model,
@@ -75,11 +95,35 @@ def test_all(debug=False):
         print(inp, out)
         print(inp.shape, out.shape)
         print(inp.shape[0])
-    model = build_model_r3(inp.shape[1], 1)
-    model.fit(inp, out, epochs=200)
+    model = build_model_a1(inp.shape[1], 1)
+    model.fit(inp, out, epochs=100)
     model_predict(model, inp, out)
 
 
 if __name__ == '__main__':
-    test_report(verbose=True)
-    test_all(debug=True)
+    # test_report(verbose=True)
+    # test_all(debug=False)
+
+    grades = preprocess_all()
+    grades = np.reshape(grades, (17, 1))
+    gdf = pd.DataFrame(grades) # grades data frame
+    print(gdf)
+    print(dir(gdf))
+    gdf.plot(kind='bar', ylim=(0, 120), colormap='terrain')
+    # plt.plot(gdf, )
+    plt.ylabel('Out of 100%')
+    plt.xlabel('Grade Percentages')
+    plt.show()
+
+    # grades = preprocess_all()
+    # grades.sort()
+    # grades = np.reshape(grades, (17, 1))
+    # gdf = pd.DataFrame(grades)  # grades data frame
+    # gdf.plot(kind='bar', ylim=(0, 115))
+    # # plt.plot(gdf, )
+    # plt.title('Grades sorted plotted')
+    # plt.ylabel('Out of 100%')
+    # plt.xlabel('Grade Percentages')
+    # plt.show()
+
+    pass
